44a76ab089eb975e8b4855432d0622f697baf756
Jeremy R. Groves, Ph.D <jgroves@niu.edu>
2020-11-18 11:04:25 -0600

    Finished Initial Program

--- Build/Code/Comparison Lists.R
+++ Build/Code/Comparison Lists.R
@@ -7,28 +7,31 @@ library(tidyverse)
 library(rvest)
 
 #Journals from IDEA
-  ideas<-read.csv("./Build/Data/ideas.csv", as.is=TRUE)
+  ideas<-read.csv("./Build/Data/ideas.csv", as.is=TRUE,header=FALSE)
     ideas$IDEA<-"X"
-
+    names(ideas)<-c("Title","ISSN","IDEA")
+      ideas$Title<-str_to_title(ideas$Title)
+      ideas$Dates<-NULL
+    
 #Jounrals from Scimago (https://www.scimagojr.com/)  
   sci<-read.csv("./Build/Data/scimag2019.csv", sep=";", as.is=TRUE)
     sci<-sci[which(sci$Type=="journal"),]
     sci<-sci[,c("Title","Issn")]  
       c<-as.data.frame(str_split(sci$Issn, ", ",simplify=TRUE))
         temp1<-data.frame(sci[,1],c[,1])
-          names(temp1)<-c("Journal","ISBN")
+          names(temp1)<-c("Journal","ISSN")
         temp2<-data.frame(sci[,1],c[,2])
           temp2[which(temp2[,2]==""),2]<-NA
-          names(temp2)<-c("Journal","ISBN")
+          names(temp2)<-c("Journal","ISSN")
           temp2<-temp2[!is.na(temp2[,2]),]
         temp3<-data.frame(sci[,1],c[,2])
           temp3[which(temp3[,2]==""),2]<-NA
           temp3<-temp3[!is.na(temp3[,2]),]
-          names(temp3)<-c("Journal","ISBN")
+          names(temp3)<-c("Journal","ISSN")
           
       sci<-rbind(temp1,temp2,temp3)
         rm(temp1, temp2, temp3)
-      sci$ISBN<-paste0(substr(sci$ISBN,1,4),"-",substr(sci$ISBN,5,8))    
+      sci$ISSN<-paste0(substr(sci$ISSN,1,4),"-",substr(sci$ISSN,5,8))    
           
   sci$SCIMAGO<-"X"
 
@@ -48,26 +51,39 @@ library(rvest)
   names(aea)<-c("Journal","ISBN")
     aea$ISBN<-trimws(gsub("ISSN: ","",aea$ISBN))
     aea$AEA<-"X"
+  
+    #Fixes what missing ISSNs are avaliable
+      temp<-read.csv(file="./Build/Data/aea missing issn.csv", header=TRUE)
+      aea<-rbind(aea,temp)
+        aea$ISBN[which(aea$ISBN=="ISSN:")]<-NA
+        aea$ISBN[which(aea$ISBN=="")]<-NA
+      aea<-aea[which(!is.na(aea$ISBN)),]
+      names(aea)<-c("Journal","ISSN","AEA")
+  
 
 #Journal from Journal Citation Index: Web of Science
-  jci<-read.csv("./Build/Data/jci2019.csv",as.is=TRUE, header=FALSE)[-1,] 
+  jci<-read.csv("./Build/Data/jci2019.csv",as.is=TRUE, header=FALSE)[-1,]
     names(jci)<-jci[1,]
-    names(jci)[which(names(jci)=="ISSN")]<-"ISBN"
       jci<-jci[2:nrow(jci),]
-    jci<-jci[,c("Full Journal Title","ISBN")]
+    jci<-jci[,c("Full Journal Title","ISSN")]
     jci$JCI<-"X"
     
-journals<-Reduce(function(x,y) merge(x,y,by="ISBN",all=TRUE), list(aea,sci,jci))
+journals<-Reduce(function(x,y) merge(x,y,by="ISSN",all=TRUE), list(aea,sci,jci))
 
   journals$Title<-journals$Journal.x
     journals$Title[is.na(journals$Title)]<-journals$Journal.y[is.na(journals$Title)]
     journals$Title[is.na(journals$Title)]<-journals$`Full Journal Title`[is.na(journals$Title)]
     journals<-journals[which(journals$Title!=""),]
     journals<-journals[!is.na(journals$Title),]
-  journals<-journals[,c("Title","ISBN","AEA","SCIMAGO","JCI")]
+  journals<-journals[,c("Title","ISSN","AEA","SCIMAGO","JCI")]
   journals<-journals[order(journals$AEA,journals$Title),]
   journals<-unique(journals)
+  
+  
+  
+journals<-journals[!duplicated(journals$ISSN),]
+  
 
 write.csv(journals,file="./Build/Output/journals.csv",row.names = FALSE)
-  
+
                  
--- README.txt
+++ README.txt
@@ -8,4 +8,15 @@ Nov. 12:
   See if IDEAs has ISSN anywhere because now they do not
   Need to be able to obtain metrics for all journals
   
-  
+
+Methodology:
+
+For AEA Econlit journal list, the website https://www.aeaweb.org/econlit/journal_list.php
+is read and then the journal names and ISSNs are pulled. For those missing ISSNs we looked
+up the ISSNs on https://portal.issn.org. For those still missing ISSNs the journals have a
+very limited run that is tracted with AEA so those journals are deleted.
+
+For the Repec/IDEAS list, the journal list at https://ideas.repec.org/top/top.journals.all.html
+is copied and cleaned in excel and then loaded directly into R. This was due to the complications
+in getting a clean version into R directly. The other problem with this dataset is there are no ISSNs 
+listed so we are required to join on name
